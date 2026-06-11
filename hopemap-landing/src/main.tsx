276f5689e0e330671021f28cd25f7797b9f6c5aa
ShiranGiladi <shirang16@gmail.com>
2026-06-11 20:27:26 +0300

    adding hopmap landing page

---
+++ hopemap-landing/src/main.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import App from "./App";
+
+import "./styles/global.css";
+import "./styles/variables.css";
+
+import { ThemeProvider } from "./context/ThemeContext";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+);
